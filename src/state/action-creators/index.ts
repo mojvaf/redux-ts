6118cebdf56f65622710fbbb01de69022fd965c2
mojvaf <mvafa44@gmail.com>
2021-11-22 14:43:29 +0100

    applying dispatch

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,9 +1,10 @@
 import axios from "axios";
 import { ActionType } from "../action-types";
 import { Action } from "../actions";
+import { Dispatch } from "redux";
 
 export const searchRepositories = (term: string) => {
-  return async (dispatch: any) => {
+  return async (dispatch: Dispatch<Action>) => {
     dispatch({
       type: ActionType.SEARCH_REPOSITORIES,
     });
@@ -24,10 +25,10 @@ export const searchRepositories = (term: string) => {
         type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
         payload: names,
       });
-    } catch (error) {
+    } catch (err: any) {
       dispatch({
         type: ActionType.SEARCH_REPOSITORIES_ERROR,
-        payload: error,
+        payload: err.message,
       });
     }
   };
